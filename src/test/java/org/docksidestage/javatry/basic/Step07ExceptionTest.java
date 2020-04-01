29eede18cafa3e35df8c0221dc62d6aab631294e
shin-kinoshita <shin.kinoshita@bizreach.co.jp>
2020-04-01 20:04:34 +0900

    solve Checked Exception in Step07ExceptionTest.java

--- src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
@@ -15,6 +15,9 @@
  */
 package org.docksidestage.javatry.basic;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.docksidestage.bizfw.basic.supercar.SupercarClient;
 import org.docksidestage.javatry.basic.st7.St7ConstructorChallengeException;
 import org.docksidestage.unit.PlainTestCase;
@@ -90,6 +93,11 @@ public class Step07ExceptionTest extends PlainTestCase {
      * (new java.io.File(".") の canonical path を取得してログに表示、I/Oエラーはメッセージとスタックトレースを代わりに)
      */
     public void test_exception_checkedException_basic() {
+        try {
+            log(new File(".").getCanonicalPath());
+        } catch (IOException e) {
+            log("IOException thrown: " + e.getMessage());
+        }
     }
 
     // ===================================================================================
